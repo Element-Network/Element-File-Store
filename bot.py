31d44b93dbaf7e3522fa1ea1896a4170bc5f317e
Shadow_Blank <ramdhananjay05@gmail.com>
2025-05-17 08:33:27 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -93,7 +93,7 @@ async def start():
     await web.TCPSite(app, bind_address, PORT).start()
     if CLONE_MODE == True:
         await restart_bots()
-    print("Bot Started Powered By ")
+    print("Bot Started Powered By")
     await idle()
 
 # Don't Remove Credit Tg - @VJ_Botz
